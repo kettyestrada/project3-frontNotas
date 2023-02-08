326a70b7f2c5008b4ec012f997e7ce5215397e71
kettyestrada <kettyestrada1519@gmail.com>
2023-02-08 22:09:05 +0100

    Actualizando con ficheros notas

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import './App.css';
 import { ThemeProvider } from './ThemeContext';
 import Header from './Header';
 import Content from './Content';
+import CreateNote from './CreateNote';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { ListNotes } from './ListNotes';
 
 function App() {
   return (
-    <ThemeProvider>
-      <Header />
-      <Content />
-    </ThemeProvider>
+    <>
+      <BrowserRouter>
+        <ThemeProvider>
+          <Header />
+          <nav>
+            <ul>
+              <li>
+                <Link to='/createnote'>create note</Link>
+              </li>
+              <li>
+                <Link to='/listnotes'>List notes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Content />
+          <Routes>
+            <Route path='/createnote' element={<CreateNote />} />
+            <Route path='/listnotes' element={<ListNotes />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </>
   );
 }
 
