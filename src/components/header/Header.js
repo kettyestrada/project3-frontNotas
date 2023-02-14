c509b540f3ed2e638469965430da9f952b50c037
VIRGINIA PEÑA <virginiapena@MacBook-Pro-de-MAURICIO.local>
2023-02-15 00:45:40 +0100

    Cerrar Sesion

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,18 @@
 import Title from '../../Title';
 import { NavLink } from 'react-router-dom';
+import { useToken } from '../../TokenContext';
 
 function Header() {
+  const [token, setToken] = useToken();
   return (
     <header>
       <NavLink to="/">
         <Title />
+        {token && (
+          <div className="button" onClick={() => setToken(null)}>
+            <p>Cerrar Sesión</p>
+          </div>
+        )}
       </NavLink>
     </header>
   );
