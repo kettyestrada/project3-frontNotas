522cfcc852df07bfc056a7ea8b113d647da2e517
kettyestrada <kettyestrada1519@gmail.com>
2023-03-01 20:12:33 +0100

    arreglos de modificar categoria

--- src/components/Note/CreateNote.js
+++ src/components/Note/CreateNote.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './Note.css';
 
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
 import Dropzone from 'react-dropzone';
 
 import { useState } from 'react';
@@ -11,167 +9,174 @@ import { useNavigate, Navigate } from 'react-router-dom';
 import { showAlert, showSuccess } from '../../functions';
 
 function CreateNote({ categories }) {
-  const [token] = useToken();
-  const navigate = useNavigate();
+    const [token] = useToken();
+    const navigate = useNavigate();
 
-  const [title, setTitle] = useState('');
-  const [text, setText] = useState('');
-  const [idCategory, setIdCategory] = useState('');
-  const [isPublic, setIsPublic] = useState('0');
-  const [photo, setPhoto] = useState(null);
-  const [loading, setLoading] = useState(false);
+    const [title, setTitle] = useState('');
+    const [text, setText] = useState('');
+    const [idCategory, setIdCategory] = useState(categories[0].id);
+    const [isPublic, setIsPublic] = useState('0');
+    const [photo, setPhoto] = useState(null);
 
-  //No permite visualizar esta pantalla si el usuario no está logueado
-  if (!token) return <Navigate to="/" />;
+    //No permite visualizar esta pantalla si el usuario no está logueado
+    if (!token) return <Navigate to='/' />;
 
-  const handleFileDrop = (files) => {
-    if (files.length > 0 && files[0].type.startsWith('image/')) {
-      setPhoto(files[0]);
-    } else {
-      showAlert('Solo se permite cargar imágenes', 'warning');
-    }
-  };
+    const handleFileDrop = (files) => {
+        if (files.length > 0 && files[0].type.startsWith('image/')) {
+            setPhoto(files[0]);
+        } else {
+            showAlert('Solo se permite cargar imágenes', 'warning');
+        }
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    const formData = new FormData();
+        const formData = new FormData();
 
-    formData.append('title', title);
-    formData.append('text', text);
-    formData.append('idCategory', Number(idCategory));
-    formData.append('isPublic', isPublic);
-    formData.append('file', photo);
+        formData.append('title', title);
+        formData.append('text', text);
+        formData.append('idCategory', Number(idCategory));
+        formData.append('isPublic', isPublic);
+        formData.append('file', photo);
 
-    try {
-      const resp = await fetch('http://localhost:8080/note', {
-        method: 'POST',
-        headers: {
-          Authorization: token,
-        },
-        body: formData,
-      });
+        try {
+            const resp = await fetch('http://localhost:8080/note', {
+                method: 'POST',
+                headers: {
+                    Authorization: token,
+                },
+                body: formData,
+            });
 
-      if (resp.status === 201) {
-        showSuccess('Nota creada satisfactoriamente');
-        navigate('/noteslist');
-      } else {
-        const data = await resp.json();
-        console.log(data);
-        showAlert('Error al crear una nota: ' + data.message, 'warning');
-      }
-    } catch (error) {
-      showAlert('Error al crear una nota: ' + error.message, 'warning');
-    }
-  };
+            if (resp.status === 201) {
+                showSuccess('Nota creada satisfactoriamente');
+                navigate('/noteslist');
+            } else {
+                const data = await resp.json();
+                console.log(data);
+                showAlert(
+                    'Error al crear una nota: ' + data.message,
+                    'warning'
+                );
+            }
+        } catch (error) {
+            showAlert('Error al crear una nota: ' + error.message, 'warning');
+        }
+    };
 
-  return (
-    <main>
-      <div className="note">
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-75">
-              <input
-                type="text"
-                id="title"
-                placeholder="Escribe aquí tú título..."
-                maxLength={100}
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                autoFocus
-                required
-              />
+    return (
+        <main>
+            <div className='note'>
+                <form onSubmit={handleSubmit}>
+                    <div className='row'>
+                        <div className='col-75'>
+                            <input
+                                type='text'
+                                id='title'
+                                placeholder='Escribe aquí tú título...'
+                                maxLength={100}
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                                autoFocus
+                                required
+                            />
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col-25'>
+                            <label htmlFor='category'>Categoria</label>
+                        </div>
+                        <div className='col-75'>
+                            <select
+                                id='category'
+                                value={idCategory}
+                                onChange={(e) => setIdCategory(e.target.value)}
+                                required
+                            >
+                                {categories.map((currentCategory) => {
+                                    return (
+                                        <option
+                                            key={currentCategory.id}
+                                            value={currentCategory.id}
+                                        >
+                                            {currentCategory.title}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='text-note'>
+                            <textarea
+                                placeholder='Escribe el texto de tú nota aquí..'
+                                maxLength={3000}
+                                value={text}
+                                onChange={(e) => setText(e.target.value)}
+                                required
+                            ></textarea>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-25'>
+                            <label htmlFor='isPublic'>Es pública?</label>
+                        </div>
+                        <div className='col-75'>
+                            <input
+                                type='radio'
+                                id='public'
+                                name='isPublic'
+                                value='public'
+                                onChange={(e) => setIsPublic('1')}
+                            />
+                            <label htmlFor='html'>Si</label>
+                            <input
+                                type='radio'
+                                id='public'
+                                name='isPublic'
+                                value='private'
+                                onChange={(e) => setIsPublic('0')}
+                                defaultChecked
+                            />
+                            <label htmlFor='html'>No</label>
+                        </div>
+                    </div>
+                    {!photo && (
+                        <Dropzone onDrop={handleFileDrop}>
+                            {({ getRootProps, getInputProps }) => (
+                                <div {...getRootProps()}>
+                                    <input
+                                        {...getInputProps()}
+                                        onChange={(e) =>
+                                            setPhoto(e.target.files[0])
+                                        }
+                                    />
+                                    <div className='dropzone'>
+                                        Arrastra una imagen aquí o haz clic para
+                                        seleccionar un archivo.
+                                    </div>
+                                </div>
+                            )}
+                        </Dropzone>
+                    )}
+                    {photo && (
+                        <img
+                            src={URL.createObjectURL(photo)}
+                            width='200'
+                            height='auto'
+                            alt='Imagen seleccionada'
+                        />
+                    )}
+
+                    <div className='row'>
+                        <input type='submit' value='Crear' />
+                    </div>
+                </form>
             </div>
-          </div>
-
-          <div className="row">
-            <div className="col-25">
-              <label htmlFor="category">Categoria</label>
-            </div>
-            <div className="col-75">
-              <select
-                id="category"
-                value={idCategory}
-                onChange={(e) => setIdCategory(e.target.value)}
-                required
-              >
-                {categories.map((currentCategory) => {
-                  return (
-                    <option key={currentCategory.id} value={currentCategory.id}>
-                      {currentCategory.title}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-          </div>
-          <div className="row">
-            <div className="text-note">
-              <textarea
-                placeholder="Escribe el texto de tú nota aquí.."
-                maxLength={3000}
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                required
-              ></textarea>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-25">
-              <label htmlFor="isPublic">Es pública?</label>
-            </div>
-            <div className="col-75">
-              <input
-                type="radio"
-                id="public"
-                name="isPublic"
-                value="public"
-                onChange={(e) => setIsPublic('1')}
-              />
-              <label htmlFor="html">Si</label>
-              <input
-                type="radio"
-                id="public"
-                name="isPublic"
-                value="private"
-                onChange={(e) => setIsPublic('0')}
-                defaultChecked
-              />
-              <label htmlFor="html">No</label>
-            </div>
-          </div>
-          {!photo && (
-            <Dropzone onDrop={handleFileDrop}>
-              {({ getRootProps, getInputProps }) => (
-                <div {...getRootProps()}>
-                  <input
-                    {...getInputProps()}
-                    onChange={(e) => setPhoto(e.target.files[0])}
-                  />
-                  <div className="dropzone">
-                    Arrastra una imagen aquí o haz clic para seleccionar un
-                    archivo.
-                  </div>
-                </div>
-              )}
-            </Dropzone>
-          )}
-          {photo && (
-            <img
-              src={URL.createObjectURL(photo)}
-              width="200"
-              height="auto"
-              alt="Imagen seleccionada"
-            />
-          )}
-
-          <div className="row">
-            <input type="submit" value="Crear" />
-          </div>
-        </form>
-      </div>
-    </main>
-  );
+        </main>
+    );
 }
 
 export default CreateNote;
